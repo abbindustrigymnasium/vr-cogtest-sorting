ec9b17916f73edac087277af2bb72249b93d4bec
abbwiljoh <william.johansson@abbindustrigymnasium.se>
2022-03-17 14:58:40 +0100

    presentation?

--- VR-Sorting/Assets/Scenes/William/Scripts/wstartButton.cs
+++ VR-Sorting/Assets/Scenes/William/Scripts/wstartButton.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class wstartButton : MonoBehaviour
 {
+    // Start is called before the first frame update
     public GameObject menu;
-    public wtimer timer;
+    public timer timer;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public void button()
     {
-        Debug.Log("Button");
         menu.active = false;
         timer.stopTimer = false;
+        SceneManager.LoadScene("william-room", LoadSceneMode.Single);
     }
 }
